759e5818380ef4a16ad50555eccf3fa9bb10f9f0
Diverbriz <lkue.steam@mail.ru>
2023-10-01 14:47:20 +0300

    Initial commit

---
+++ src/test/java/com/example/woyfitserver/WoyFitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woyfitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoyFitServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
