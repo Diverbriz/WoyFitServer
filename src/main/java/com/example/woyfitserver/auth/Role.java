7d9286b8d963150ecfda6a3654fdd6c7875433d1
Diverbriz <lkue.steam@mail.ru>
2023-12-12 11:49:03 +0300

    Fix hasRole bug

--- src/main/java/com/example/woyfitserver/auth/Role.java
+++ src/main/java/com/example/woyfitserver/auth/Role.java
@@ -2,22 +2,33 @@ package com.example.woyfitserver.auth;
 
 import com.example.woyfitserver.user.User;
 import jakarta.persistence.*;
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
+
 @Entity
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @Transient
-    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.EAGER)
     private Set<User> users = new HashSet<>();
 
     public Role(){}
@@ -45,6 +56,6 @@ public class Role implements GrantedAuthority {
 
     @Override
     public String getAuthority() {
-        return null;
+        return name;
     }
 }
--- src/main/java/com/example/woyfitserver/user/User.java
+++ src/main/java/com/example/woyfitserver/user/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
     private boolean isActiveAccount;
 
     @Getter
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = {@JoinColumn(name = "user_id")}, inverseJoinColumns = {@JoinColumn(name = "role_id")})
     private Set<Role> roles;
