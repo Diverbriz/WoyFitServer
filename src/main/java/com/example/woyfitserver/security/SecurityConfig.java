4f4b014b70fb1bff8ac8ed8ce636ed5a9a3e87d6
Diverbriz <lkue.steam@mail.ru>
2024-01-08 20:27:52 +0300

    Migration to h2 database

--- src/main/java/com/example/woyfitserver/security/SecurityConfig.java
+++ src/main/java/com/example/woyfitserver/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.woyfitserver.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,12 +23,10 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 public class SecurityConfig {
     private final AuthenticationConfiguration authConfiguration;
     private final JwtFilter jwtFilter;
-
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return authConfiguration.getAuthenticationManager();
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(new HandlerMappingIntrospector());
@@ -42,7 +42,7 @@ public class SecurityConfig {
                                 .requestMatchers(mvcMatcherBuilder.pattern("/**")).permitAll()
                                 .requestMatchers(PathRequest.toH2Console()).authenticated()
                                 .anyRequest().authenticated()
-//                        .requestMatchers("/auth/test").hasRole("USER")
+//                        .requestMatchers("/auth/test", "/api/**").hasRole("USER")
 //                        .requestMatchers("/auth/**").anonymous()
 //                        .anyRequest().authenticated()
                 ).addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class);
